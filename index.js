7bdc796b5b721a506bd65b9460189febec006cdc
stanleyz1227 <zhangs5@bu.edu>
2022-06-12 18:49:32 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,207 @@
+const { Client, Intents, Message, MessageActionRow, MessageButton} = require('discord.js');
+const { token } = require('./config.json');
+const { TicTacToe } = require('./databaseObjects.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.on('messageCreate', (message) => {
+    if (message.author.id === client.user.id) return;
+
+    if (message.content === "ping") {
+        message.reply("pong");
+    }
+});
+
+/* TicTacToe */
+
+let EMPTY = Symbol("empty");
+let PLAYER = Symbol("player");
+let BOT = Symbol("bot");
+
+let tictactoe_state
+
+function makeGrid() {
+    components = []
+
+    for (let row = 0; row < 3; row++) {
+        actionRow = new MessageActionRow()
+
+        for (let col = 0; col < 3; col++) {
+            messageButton = new MessageButton()
+                .setCustomId('tictactoe_' + row + '_' + col)
+
+            switch(tictactoe_state[row][col]) {
+                case EMPTY:
+                    messageButton
+                        .setLabel(' ')
+                        .setStyle('SECONDARY')
+                    break;
+                case PLAYER:
+                    messageButton
+                        .setLabel('X')
+                        .setStyle('PRIMARY')
+                    break;
+                case BOT:
+                    messageButton
+                        .setLabel('O')
+                        .setStyle('DANGER')
+                    break;
+            }
+
+            actionRow.addComponents(messageButton)
+        }
+
+        components.push(actionRow)
+    }
+    
+    return components
+}
+
+function getRandInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function isDraw() {
+    for (let row = 0; row < 3; row++) {
+        for (let col = 0; col < 3; col++) {
+            if (tictactoe_state[row][col] == EMPTY)
+                return false;
+        }
+    }
+    console.log("draw")
+    return true;
+}
+
+function isGameOver() {
+    for (let i = 0; i < 3; i++) {
+        if ((tictactoe_state[i][0] == tictactoe_state[i][1]) && (tictactoe_state[i][1] == tictactoe_state[i][2]) && (tictactoe_state[i][2] != EMPTY)) {
+            return true;
+        }
+            
+        if ((tictactoe_state[0][i] == tictactoe_state[1][i]) && (tictactoe_state[1][i] == tictactoe_state[2][i]) && (tictactoe_state[2][i] != EMPTY)) {
+            return true;
+        }    
+    }
+
+    if (tictactoe_state[1][1] != EMPTY) {
+        if ((tictactoe_state[0][0] == tictactoe_state[1][1] && tictactoe_state[1][1] == tictactoe_state[2][2]) || (tictactoe_state[0][2] == tictactoe_state[1][1] && tictactoe_state[1][1] == tictactoe_state[2][0])) {
+            return true;
+        } 
+    }
+    return false;
+}
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isButton()) return;
+    if (!interaction.customId.startsWith('tictactoe')) return;
+
+    // if the game was already finished, grid should not be clickable
+    if (isGameOver()) {
+        interaction.update({
+            components: makeGrid()
+        })
+        return;
+    }
+
+    parsedFields = interaction.customId.split("_")
+    row = parsedFields[1]
+    col = parsedFields[2]
+
+    if (tictactoe_state[row][col] != EMPTY) {
+        interaction.update({
+            content: "You cannot select that square!",
+            components: makeGrid()
+        })
+        return;
+    }
+
+    tictactoe_state[row][col] = PLAYER;
+
+    // check if game is a draw or won after player move
+    if (isGameOver()) {
+
+        let user = await TicTacToe.findOne({
+            where: {
+                user_id: interaction.user.id
+            }
+        });
+
+        if (!user) {
+            user = await TicTacToe.create({
+                user_id: interaction.user.id
+            })
+        }
+        
+        await user.increment('score');
+
+        interaction.update({
+            content: "You've won a game! Current wins: " + (user.get('score') + 1),
+            components: makeGrid()
+        })
+        return;
+    }
+    if (isDraw()) {
+        interaction.update({
+            content: "The game resulted in a draw!",
+            components: []
+        })
+        return;
+    }
+
+    /* Bot Functionality */
+    let botRow
+    let botCol
+    do {
+        botRow = getRandInt(3)
+        botCol = getRandInt(3)
+        //console.log(botRow)
+        //console.log(botCol)
+    } while(tictactoe_state[botRow][botCol] != EMPTY);
+    
+    tictactoe_state[botRow][botCol] = BOT;
+
+    // check if game is a draw or won after bot move
+    if (isGameOver()) {
+        interaction.update({
+            content: "You lost!",
+            components: makeGrid()
+        })
+        return;
+    }
+    if (isDraw()) {
+        interaction.update({
+            content: "The game resulted in a draw!",
+            components: []
+        })
+        return;
+    }
+
+    interaction.update({
+        components: makeGrid()
+    })
+})
+
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === 'tictactoe') {
+        tictactoe_state = [
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]
+        ];
+
+        await interaction.reply({ content: 'Playing a game of tic tac toe!', components: makeGrid()} )
+    }
+});
+
+/* */
+
+client.login(token);
